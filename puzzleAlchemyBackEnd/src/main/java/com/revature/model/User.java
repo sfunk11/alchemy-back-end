1eacfe2c56d8b13319da05b77a2382d035f8e6a8
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-10-29 08:59:35 -0400

    integrated Mike's project code into puzzleAlchemyBackEnd

---
+++ puzzleAlchemyBackEnd/src/main/java/com/revature/model/User.java
@@ -0,0 +1,119 @@
+package com.revature.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+	@Column(name="user_id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int userID;
+	
+	@Column(name="user_uname", unique=true, nullable=false)
+	private String uName;
+	
+	@Column(name="user_email")
+	private String email;
+	
+	@Column(name="user_fname")
+	private String fname;
+	
+	@Column(name="user_lname")
+	private String lname;
+	
+	@Column(name="user_role")
+	private int roleID;
+	
+	public User() {
+		
+	}
+
+	public User(int userID, String uName, String email, String fname, String lname, int roleID) {
+		super();
+		this.userID = userID;
+		this.uName = uName;
+		this.email = email;
+		this.fname = fname;
+		this.lname = lname;
+		this.roleID = roleID;
+	}
+
+	public User(String uName, String email, String fname, String lname, int roleID) {
+		super();
+		this.uName = uName;
+		this.email = email;
+		this.fname = fname;
+		this.lname = lname;
+		this.roleID = roleID;
+	}
+
+	public User(String uName, String password) {
+		super();
+		this.uName = uName;
+		this.email = email;
+	}
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public String getuName() {
+		return uName;
+	}
+
+	public void setuName(String uName) {
+		this.uName = uName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public int getRoleID() {
+		return roleID;
+	}
+
+	public void setRoleID(int roleID) {
+		this.roleID = roleID;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userID=" + userID + ", uName=" + uName + ", email=" + email + ", fname=" + fname
+				+ ", lname=" + lname + ", roleID=" + roleID + "]";
+	}
+	
+	
+
+}
---
+++ puzzleAlchemyBackEnd/src/main/java/com/revature/service/UserService.java
@@ -0,0 +1,59 @@
+package com.revature.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.model.User;
+import com.revature.repository.UserRepo;
+
+@Service
+public class UserService {
+	
+	private UserRepo uRepo;
+	
+	public UserService() {
+		
+	}
+	
+	@Autowired
+	public UserService(UserRepo uRepo) {
+		super();
+		this.uRepo = uRepo;
+	}
+	
+	public List<User> getAllUsers(){
+		return uRepo.findAll();
+	}
+	
+	public void insertUser(User user) {
+		uRepo.save(user);
+	}
+	
+	public User getUserByUsername(String username) {
+		return uRepo.findByuName(username);
+	}
+	
+	public List<User> getUserByRoleID(int id){
+		return uRepo.findByRoleID(id);
+	}
+	
+	public User getUserByUserID(int id) {
+		return uRepo.findByUserID(id);
+	}
+	
+	public User getUserByUsernameAndEmail(String username, String email) {
+		return uRepo.findByuNameAndEmail(username, email);
+	}
+	
+	public void deleteUser(User user) {
+		uRepo.delete(user);
+	}
+	
+	public void updateUser(User user) {
+		uRepo.save(user);
+	}
+
+}
+
