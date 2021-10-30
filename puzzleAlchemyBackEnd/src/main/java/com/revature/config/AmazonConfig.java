1f88aae8c397748ac31109a6b02e39c186a07ffa
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-10-30 16:02:27 -0400

    changed AWS creds and abstracted them

--- puzzleAlchemyBackEnd/src/main/java/com/revature/config/AmazonConfig.java
+++ puzzleAlchemyBackEnd/src/main/java/com/revature/config/AmazonConfig.java
@@ -5,15 +5,37 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonConfig {
+   
+	ClassLoader cl = getClass().getClassLoader();
+	InputStream is;
+	Properties p= new Properties();
+    
+	public AmazonConfig() {
+		is = cl.getResourceAsStream("aws.properties");
+		try {
+			p.load(is);
+		}catch(IOException e) {
+			
+			
+		}
+	}
+    
     @Bean
     public AmazonS3 s3() {
+    	String key = p.getProperty("AWS_ACCESS_KEY_ID");
+    	String secret = p.getProperty("AWS_SECRET_ACCESS_KEY");
         AWSCredentials awsCredentials =
-                new BasicAWSCredentials("AKIAVSFIGKVH4DGLXLN6", "kNxEWoe0dAA9vxRIzM/c6T4FaUskpS9/Kz/uQxvt");
+                new BasicAWSCredentials(key, secret);
         return AmazonS3ClientBuilder
                 .standard()
                 .withRegion("us-east-2")
